3eddf620b0ba02770f87a0e57efb5738c39e7006
borodinchik <borodin.vladymyr@gmail.com>
2018-04-24 21:58:29 +0300

    Seeding part Product and Review

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -2,8 +2,14 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Model\Product::class, function (Faker $faker) {
+use App\Model\Product;
+
+$factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(100, 1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2, 30)
     ];
 });
--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -2,8 +2,16 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Model\Review::class, function (Faker $faker) {
+use App\Model\Product;
+use App\Model\Review;
+
+$factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function(){
+            return Product::all()->random();
+        },
+        'costumer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0, 5)
     ];
 });
