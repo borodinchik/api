14351fd62340e1c9944e26044456b9b29abb5399
borodinchik <borodin.vladymyr@gmail.com>
2018-04-26 21:11:59 +0300

    Relationship create

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -4,7 +4,15 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Product extends Model
 {
-    //
+    protected $table = 'products';
+
+   protected $fillable = ['name', 'detail', 'price', 'stock', 'discount'];
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -4,7 +4,17 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Review extends Model
 {
-    //
+    protected $table = 'reviews';
+
+    protected $fillable = [
+        'product_id', 'costumer', 'review', 'star'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
