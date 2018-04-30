7e68cc32ecaa504c810680a3b349f55358172be1
borodinchik <borodin.vladymyr@gmail.com>
2018-04-30 10:26:59 +0300

    Review Update & Destroy

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -15,6 +15,7 @@ class ReviewResource extends Resource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'costumer' => $this->costumer,
             'body' => $this->review,
             'star' => $this->star,
